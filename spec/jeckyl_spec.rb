cc9c68d87ea756ddb973093aa45c6312622fae74
Robert Sharp <robert@osburn-sharp.ath.cx>
2011-04-08 23:42:36 +0100

    slight update to baseline

--- spec/jeckyl_spec.rb
+++ spec/jeckyl_spec.rb
@@ -2,7 +2,7 @@ require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 require 'jeckyl/errors'
 require File.expand_path(File.dirname(__FILE__) + '/../test/test_configurator')
 
-conf_path = File.expand_path(File.dirname(__FILE__) + '/../conf.d')
+conf_path = File.expand_path(File.dirname(__FILE__) + '/../test/conf.d')
 
 describe "Jeckyl" do
 
